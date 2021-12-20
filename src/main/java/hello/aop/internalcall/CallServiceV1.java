709c94d0f1c7b4c14c209a5d0b2588e08ea53e5e
mybabygrand <amuro_ray@kakao.com>
2021-12-20 22:41:32 +0900

    Section 13. Proxy and Internal call - start  - Proxy and Internal call, self injection

---
+++ src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,28 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        log.info("[setCallServiceV1] {}", callServiceV1.getClass());
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external(){
+        log.info("call external");
+        callServiceV1.internal();
+    }
+
+    public void internal(){
+        log.info("call internal");
+    }
+}
